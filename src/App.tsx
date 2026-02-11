3ad5c2d6f239b3652cd43ea036dff748e32d4fb0
Fernando Zárate <150300719+fernandozarate07@users.noreply.github.com>
2026-02-11 20:37:12 -0300

    feat: update font styles and enhance layout in App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,29 +5,39 @@ function App() {
     <div className="layout">
       <footer className="footer">
         <div className="footer__wrapper">
-          <p className="footer__p">© Fernando Zárate</p>
+          <p className="footer__p">&copy; Fernando Zárate</p>
         </div>
       </footer>
       <main className="main">
         <div className="main__container--left">
           <div className="hero">
-            <h1 className="name">Fernando Zárate</h1>
+            <h1 className="name">
+              <span className="name__initial">F</span>ernando <span className="name__initial">Z</span>árate
+            </h1>
             <h2 className="subtitle">Developer & Designer</h2>
           </div>
           <div className="about">
             <p className="about__p">
-              Desde San Juan, Argentina, diseño, compongo, construyo. No distingo entre un acorde, un trazo o una línea
-              de código; para mí, todo es el mismo impulso de <span className="highlight">materializar</span> lo que
-              antes no existía.
+              <span className="highlight__initial">O</span>riginario de San Juan, Argentina, soy diseñador,
+              desarrollador y músico. Para mí, un acorde, un trazo o una línea de código representan lo mismo: un
+              impulso de <span className="highlight">materializar</span> lo que antes no existía.
             </p>
             <p className="about__p">
-              Persigo el <span className="highlight">equilibrio</span> entre el rigor y la estética. Me enfoco en crear
-              interfaces simples y memorables, partiendo de la usabilidad y la eficiencia para alcanzar una forma que
-              represente la <span className="highlight">identidad</span> del producto.
+              <span className="highlight__initial">P</span>ersigo el <span className="highlight">equilibrio</span> entre
+              rigor y estética. Me enfoco en crear interfaces simples y memorables, priorizando la usabilidad y la
+              eficiencia para reflejar la <span className="highlight">identidad</span> de cada producto.
             </p>
           </div>
         </div>
-        <div className="main__container--right"></div>
+        <div className="main__container--right">
+          <div className="proyect">
+            <div className="proyect__container">
+              <p className="proyect__id">01.</p>
+              <h3 className="proyect__title">Unexo</h3>
+            </div>
+            <p className="proyect__subtitle">Enero 2026 · Fullstack Developer · Designer</p>
+          </div>
+        </div>
       </main>
       <header className="header">
         <div className="header__navbar">
